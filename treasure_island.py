23b08e5e409b8d0aa0b45d883afa2618453f394d
Al Delcy <aldelcy2000@gmail.com>
2021-11-15 20:23:32 -0500

    refactored door choices

--- treasure_island.py
+++ treasure_island.py
@@ -4,6 +4,13 @@ print("Welcome to Treasure Island. Your mission is to find the treasure!")
 
 left_or_right = input("You're at a crossroad, where would you like to go? Left or Right. ").lower()
 
+colors_choice = {
+    "blue"   : "Game Over. You've wondered your way into a volcano.",
+    "red"    : "Game Over. Rabbid beasts took your body away.",
+    "yellow" : "You win! You've found the treasure!",
+    "green"  : "Game Over. You were attacked by a flock of angry brids."
+}
+
 if left_or_right == "right":
     print("Game Over. You fell off a cliff.")
 elif left_or_right == "left":
@@ -13,10 +20,5 @@ elif left_or_right == "left":
         print("Game Over. The water was piranha infested...")
     elif swim_or_wait == "wait":
         which_door = input("\n" + "A boat arrives and you head to the island." + "\n" +
-        "You arrive at the island where there are 3 doors. Which do you take? Yellow, Blue, or Red. ").lower()
-        if which_door == "red":
-            print("Game Over. Rabbid beasts took your body away.")
-        elif which_door == "blue":
-            print("Game Over. You've wondered your way into a volcano.")
-        elif which_door == "yellow":
-            print("You win! You've found the treasure!")
+        "You arrive at the island where there are 3 doors. Which do you take? " + ", ".join(colors_choice.keys()) + ":" ).lower()
+    print( colors_choice[which_door] )
